82ff328d3678f651b26049eb3386e447d95b4855
abhishin <2abhishekshinde@gmail.com>
2023-10-05 15:05:16 +0530

    enquiry

--- src/app/includes/enquiry/enquiry.component.ts
+++ src/app/includes/enquiry/enquiry.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonService } from 'src/app/modules/shared/common.service';
 
 @Component({
   selector: 'app-enquiry',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./enquiry.component.css']
 })
 export class EnquiryComponent {
+  constructor(public fb:FormBuilder,private common:CommonService) { }
+
+  enquiryForm:FormGroup;
+
+  ngOnInit(): void {
+
+    this.enquiryForm=this.fb.group({
+      
+
+
+
+
+
+      enquiryId:[],
+      // id:[],
+      customerName:['Daniel',[Validators.required]],
+      pancardNumber:['GKZPK9636P'],
+      customerMobileNumber:[8788164114],
+      customerAlternateMobileNumber:[7057279657],
+      customerEmailId:['bingopanda700@gmail.com'],
+      customerDateOfBirth:['15.01.1887'],
+      enquiryStatus:[''],
+      
+    
+
+    })
+  }
+
+  isSubmited:string=''
+
+  onSubmit()
+  {
+   
+    if(this.enquiryForm.valid)
+    {
+      this.common.saveEnquiryData(this.enquiryForm.value).subscribe();
+      // this.isSubmited='Your Enquiry Submitted successfully';
+      // document.cookie = JSON.stringify( this.enquiryForm.value);
+      window.location.reload();
+    }
+     
+
+  
+     }
 
 }
